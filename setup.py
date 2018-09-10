d972bf2808a632e0f95b4933ff495b3e9179556c
Gunnar Strand <Gurra.Strand@gmail.com>
2018-09-10 22:02:14 +0200

    Fix wrong project name in setup.py
    The name was set to "badge" but should be "abadge".
    
    Bump version to 0.2.1.
    
    Signed-off-by: Gunnar Strand <Gurra.Strand@gmail.com>

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ with open('README.rst') as fp:
     readme = fp.read()
 
 setup(
-    name='badge',
+    name='abadge',
     description='Generate badges/shields with pure HTML/CSS.',
     long_description=readme,
-    version='0.2.0',
+    version='0.2.1',
     url='https://github.com/Gustra/abadge',
     author='Gunnar Strand',
     author_email='Gurra.Strand@gmail.com',
